3758a422af135602bb3725816f0929c45c561ca7
Manas Raghuwanshi <manasr955@gmail.com>
2025-12-09 23:25:45 +0530

    fix: added new colour theme

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { MessageCircle } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { AnimatedAdoptButton } from "./animated-adopt-button";
 import FloatingPet from "./FloatingPet";
